37e454fff317c7381bc36ff4e7968982a06f11e7
Emily N <emjneil@gmail.com>
2021-07-02 09:36:18 +0100

    visualization_1Sim

--- visualization.py
+++ visualization.py
@@ -1,6 +1,6 @@
 # # # Visualizing the Knepp ABC/ABM model # # #
 from run_model import run_model
-from KneppModel_ABM import roeDeer_agent, habitatAgent
+from KneppModel_ABM import KneppModel, roeDeer_agent, habitatAgent
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import ChartModule
@@ -50,13 +50,13 @@ chart_element = ChartModule([{"Label": "Roe deer", "Color": "#666666"},
                              ])
 
 
-server = ModularServer(run_model, [canvas_element, chart_element])
-                    #    "KneppModel", {"chance_reproduceSapling":0.5, "chance_reproduceYoungScrub":0.5, "chance_regrowGrass":0.5, "chance_saplingBecomingTree":0.5, "chance_youngScrubMatures":0.5,
-                    #     "chance_scrubOutcompetedByTree":0.5, "chance_grassOutcompetedByTreeScrub":0.5, "chance_saplingOutcompetedByTree":0.5, "chance_saplingOutcompetedByScrub":0.5, "chance_youngScrubOutcompetedByScrub":0.5, "chance_youngScrubOutcompetedByTree":0.5,
-                    #     "initial_roeDeer":10, "initial_grassland":71, "initial_woodland":12, "initial_scrubland":1,
-                    #     "roeDeer_reproduce":0.5, "roeDeer_gain_from_grass":0.005, "roeDeer_gain_from_Trees":0.005, "roeDeer_gain_from_Scrub":0.005, "roeDeer_gain_from_Saplings":0.005, "roeDeer_gain_from_YoungScrub":0.005, 
-                    #     "roeDeer_impactGrass": 5, "roeDeer_saplingsEaten":100, "roeDeer_youngScrubEaten":100, "roeDeer_treesEaten":10, "roeDeer_scrubEaten":10,
-                    #     "width":10, "height":10})
+server = ModularServer(KneppModel, [canvas_element, chart_element],
+                       "KneppModel", {"chance_reproduceSapling":0.5, "chance_reproduceYoungScrub":0.5, "chance_regrowGrass":0.5, "chance_saplingBecomingTree":0.5, "chance_youngScrubMatures":0.5,
+                        "chance_scrubOutcompetedByTree":0.5, "chance_grassOutcompetedByTreeScrub":0.5, "chance_saplingOutcompetedByTree":0.5, "chance_saplingOutcompetedByScrub":0.5, "chance_youngScrubOutcompetedByScrub":0.5, "chance_youngScrubOutcompetedByTree":0.5,
+                        "initial_roeDeer":10, "initial_grassland":71, "initial_woodland":12, "initial_scrubland":1,
+                        "roeDeer_reproduce":0.5, "roeDeer_gain_from_grass":0.005, "roeDeer_gain_from_Trees":0.005, "roeDeer_gain_from_Scrub":0.005, "roeDeer_gain_from_Saplings":0.005, "roeDeer_gain_from_YoungScrub":0.005, 
+                        "roeDeer_impactGrass": 5, "roeDeer_saplingsEaten":100, "roeDeer_youngScrubEaten":100, "roeDeer_treesEaten":10, "roeDeer_scrubEaten":10,
+                        "width":10, "height":10})
 
 server.port = 8521 # The default
 server.launch()
